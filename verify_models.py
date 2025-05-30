b53d0931bad12f699f8794592e8d8fe11ee46037
xSpaceC4t <mikolajkrawczyk7sm@gmail.com>
2025-05-30 19:59:03 +0200

    Work in progress

---
+++ verify_models.py
@@ -0,0 +1,54 @@
+from archs_tinygrad.rrdbnet_arch import RRDBNet as RRDBNetTinygrad
+from archs_torch.rrdbnet_arch import RRDBNet as RRDBNetTorch
+import torch
+from tinygrad import Tensor, nn
+import numpy as np
+from tinygrad.nn.state import safe_save, safe_load, get_state_dict, load_state_dict
+from utils import *
+
+###
+
+pth_path = "weights/RealESRGAN_x4plus.pth"
+
+model_torch = RRDBNetTorch(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+loadnet = torch.load(pth_path, map_location=torch.device('cpu'))
+model_torch.load_state_dict(loadnet['params_ema'], strict=True)
+
+model_tinygrad = RRDBNetTinygrad(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+load_realesr_common_x4plus(model_tinygrad, pth_path)
+
+out_torch = model_torch(torch.ones(1, 3, 32, 32))
+out_tinygrad = model_tinygrad(Tensor.ones(1, 3, 32, 32))
+print("RealESRGAN_x4plus.pth:", np.allclose(out_torch.detach().numpy(), out_tinygrad.numpy()))
+
+###
+
+pth_path = "weights/RealESRNet_x4plus.pth"
+
+model_torch = RRDBNetTorch(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+loadnet = torch.load(pth_path, map_location=torch.device('cpu'))
+model_torch.load_state_dict(loadnet['params_ema'], strict=True)
+
+model_tinygrad = RRDBNetTinygrad(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+load_realesr_common_x4plus(model_tinygrad, pth_path)
+
+out_torch = model_torch(torch.ones(1, 3, 32, 32))
+out_tinygrad = model_tinygrad(Tensor.ones(1, 3, 32, 32))
+print("RealESRNet_x4plus.pth:", np.allclose(out_torch.detach().numpy(), out_tinygrad.numpy()))
+
+###
+
+pth_path = "weights/RealESRGAN_x4plus_anime_6B.pth"
+
+model_torch = RRDBNetTorch(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+loadnet = torch.load(pth_path, map_location=torch.device('cpu'))
+model_torch.load_state_dict(loadnet['params_ema'], strict=True)
+
+model_tinygrad = RRDBNetTinygrad(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+load_realesrgan_x4plus_anime_6b(model_tinygrad, pth_path)
+
+out_torch = model_torch(torch.ones(1, 3, 32, 32))
+out_tinygrad = model_tinygrad(Tensor.ones(1, 3, 32, 32))
+print("RealESRGAN_x4plus_anime_6B.pth:", np.allclose(out_torch.detach().numpy(), out_tinygrad.numpy()))
+
+###
