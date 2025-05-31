1eee94a91b6583530495b738a70d30a13a9ed63f
xSpaceC4t <mikolajkrawczyk7sm@gmail.com>
2025-05-31 13:39:41 +0200

    Work in progress

--- verify_models.py
+++ verify_models.py
@@ -1,5 +1,9 @@
 from archs_tinygrad.rrdbnet_arch import RRDBNet as RRDBNetTinygrad
 from archs_torch.rrdbnet_arch import RRDBNet as RRDBNetTorch
+
+from archs_tinygrad.srvgg_arch import SRVGGNetCompact as SRVGGNetCompactTinygrad
+from archs_torch.srvgg_arch import SRVGGNetCompact as SRVGGNetCompactTorch
+
 import torch
 from tinygrad import Tensor, nn
 import numpy as np
@@ -65,5 +69,21 @@ load_rrdbnet(model_tinygrad, pth_path, 23)
 out_torch = model_torch(torch.ones(1, 3, 32, 32))
 out_tinygrad = model_tinygrad(Tensor.ones(1, 3, 32, 32))
 print("RealESRGAN_x2plus.pth:", np.allclose(out_torch.detach().numpy(), out_tinygrad.numpy()))
+
+###
+
+pth_path = "weights/realesr-animevideov3.pth"
+
+model_torch = SRVGGNetCompactTorch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+loadnet = torch.load(pth_path, map_location=torch.device('cpu'))
+model_torch.load_state_dict(loadnet['params'], strict=True)
+
+model_tinygrad = SRVGGNetCompactTinygrad(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+load_srvgg(model_tinygrad, pth_path)
+
+out_torch = model_torch(torch.ones(1, 3, 32, 32))
+out_tinygrad = model_tinygrad(Tensor.ones(1, 3, 32, 32))
+
+print("realesr-animevideov3.pth:", np.allclose(out_tinygrad.numpy(), out_torch.detach().numpy(), atol=1e-5, rtol=1e-5))
 
 ###
